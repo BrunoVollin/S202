3ebb11d4b62da9dfd9ee2c20d2b03b06ece7bb45
bruno <bruno.3av@gmail.com>
2022-03-12 18:26:49 -0300

    added aggregation method

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from db.database import Database
 from helper.WriteAJson import writeAJson
 
-db = Database(database="pokedex", collection="pokemons")
+db = Database(database="mercadinho", collection="ordens_produtos")
 db.resetDatabase()
 
+result1 = db.collection.aggregate([
+    {"$group": {"_id": "$cliente", "total": {"$sum": "$total"}}},
+    {"$sort": {"total": -1}}
+])
+
+writeAJson(result1, "ascasc")
