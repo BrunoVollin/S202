e604cda4e42f900fe94b49699e2cb5aa82c518a2
bruno <bruno.3av@gmail.com>
2022-04-22 23:53:34 -0300

    fixed write a json function

--- helper/write_a_json.py
+++ helper/write_a_json.py
@@ -3,14 +3,13 @@ import os
 from bson import json_util
 
 
-def writeAJson(data, name: str):
+def write_a_json(data, name: str):
     parsed_json = json.loads(json_util.dumps(data))
 
     if not os.path.isdir("./json"):
         os.makedirs("./json")
-        
 
-    with open(f"./json/{name}.json", 'w') as json_file:
+    with open(f'./json/{name}.json', 'w') as json_file:
         json.dump(parsed_json, json_file,
                   indent=4,
                   separators=(',', ': '))
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from helper.WriteAJson import writeAJson
+from helper.write_a_json import write_a_json
 from db.database import Graph
 
-db = Graph("bolt://54.236.32.197:7687", "neo4j", "egg-diseases-war")
+
+db = Graph("bolt://3.89.131.153:7687", "neo4j", "curls-magnetos-developments")
 
 data = db.execute_query("match (n) return n")
-writeAJson(data, "all_data")
-
+write_a_json(data, "all_data")
