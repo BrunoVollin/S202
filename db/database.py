11b82d207c222fa154186effcc188c0b006592bf
Your Name <you@example.com>
2022-04-25 20:36:52 -0300

    simplified connection database class

--- db/database.py
+++ db/database.py
@@ -1,6 +1,4 @@
 from neo4j import GraphDatabase
-from neo4j.exceptions import ServiceUnavailable
-import logging
 
 
 class Graph:
@@ -17,29 +15,3 @@ class Graph:
             for record in results:
                 data.append(record)
             return data
-
-    @staticmethod
-    def _execute(tx, query):
-        try:
-            data = []
-            result = tx.run(query)
-            for record in result:
-                data.append(record)
-
-        except ServiceUnavailable as exception:
-            logging.error(f"{query} gerou um erro: \n {exception}")
-            raise
-
-        return data
-
-    def read(self, query):
-        with self.driver.session() as session:
-            result = session.read_transaction(self._execute, query)
-            return result
-
-    def write(self, query):
-        with self.driver.session() as session:
-            result = session.write_transaction(self._execute, query)
-            return result
-
-
