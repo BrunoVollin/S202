1ba9d461a8404332cdd725891474dbc91aae06e5
Your Name <you@example.com>
2022-04-25 20:37:31 -0300

    added create example at main.py

--- main.py
+++ main.py
@@ -6,3 +6,11 @@ db = Graph("bolt://3.89.131.153:7687", "neo4j", "curls-magnetos-developments")
 
 data = db.execute_query("match (n) return n")
 write_a_json(data, "all_data")
+
+
+data = db.execute_query("create (n:Person {name: 'John'}) return n")
+write_a_json(data, "all_data")
+
+
+
+
