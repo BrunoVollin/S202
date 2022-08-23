27309c85cfc7626cae5d4b2933ba6bfa532e2dc2
Lucas de Souza Resende <99922083+LucasSResende@users.noreply.github.com>
2022-08-23 00:13:37 -0300

    Relatório 03
    Atividade Relatório 03 realizada com as devidas querys criadas.

--- db/pokedex.py
+++ db/pokedex.py
@@ -59,3 +59,48 @@ class Pokedex:
                 evolutions.append(evolution['name'])
 
         return evolutions
+
+    def getPokemonsByNumber(self, num: str):
+        response = self.collection.find({"num": num},
+                                        {"_id": 0, "num": 1, "name": 1,
+                                         "next_evolution": 1, "prev_evolution": 1,
+                                         "type": 1, "weaknesses": 1})
+        result = {}
+        for pokemon in response:
+            result = pokemon
+        return result
+
+    def getPokemonsByEgg(self, egg: str):
+        response = self.collection.find({"egg": egg}, {
+            "_id": 0, "name": 1, "egg": 1,
+            "type": 1})
+        result = []
+        for pokemon in response:
+            result.append(pokemon)
+        return result
+
+    def getPokemonsByWeaknesses(self, weaknesses: list):
+        response = self.collection.find({"weaknesses": {"$all": weaknesses}}, {
+            "_id": 0, "name": 1, "weaknesses": 1})
+        result = []
+        for pokemon in response:
+            result.append(pokemon)
+        return result
+
+    def getPokemonsByHeight(self, height: list):
+        response = self.collection.find({"height": {"$all": height}}, {
+            "_id": 0, "name": 1, "height": 1, "next_evolution": 1})
+        result = []
+        for pokemon in response:
+            result.append(pokemon)
+        return result
+
+    def getPokemonsByCandyCount(self, candy_count: int):
+        response = self.collection.find({"candy_count": candy_count},
+                                        {"_id": 0, "num": 1, "name": 1,
+                                         "next_evolution": 1, "prev_evolution": 1,
+                                         "type": 1, "candy_count": 1, "weaknesses": 1})
+        result = {}
+        for pokemon in response:
+            result = pokemon
+        return result
